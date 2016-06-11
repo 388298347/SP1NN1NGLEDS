d341eb22cfbfee8fbb5785e8ffee23bf5bd6d71d
388298347 <9724763@gmail.com>
2016-06-11 16:31:41 +0200

    Update sketch_LEDansteuerung.ino

--- sketch_LEDansteuerung.ino
+++ sketch_LEDansteuerung.ino
@@ -1,27 +1,10 @@
-int ledanzahl = 12;
-LED leds[ledanzahl];
-
-void setup() {
-  for (int i = 0; i < ledanzahl; i ++)
-  {
-    pinMode(i, OUTPUT);
-    leds[i] = new LED(i);
-  }
-}
-
-void loop() {
-  
-  for (int i = 0; i < leds.length; i++)
-  leds[i].an();
-  delay(10);
-  for (int i = 0; i < leds.length; i++)
-  leds[i].aus();
-  delay(10);
-}
-
-class LED()
-{
+class LED {
   int pin;
+  public:
+  LED()
+  {
+    
+  }
   LED(int pinnum)
   {
     pin = pinnum;
@@ -42,5 +25,23 @@ class LED()
     }
     return false;
   }
+};
+  const int ledanzahl = 12;
+  LED leds[ledanzahl] = {LED(13), LED(2), LED(3), LED(4), LED(5), LED(6), LED(7), LED(8), LED(9), LED(10), LED(11), LED(12)};
+void setup() {
+  for (int i = 2; i < ledanzahl+2; i ++)
+  {
+    pinMode(i, OUTPUT);
+  }
 }
+
+void loop() {
+  for (int i = 0; i < ledanzahl; i++)
+  leds[i].an();
+  delay(10);
+  for (int i = 0; i < ledanzahl; i++)
+  leds[i].aus();
+  delay(10);
+}
+
 
