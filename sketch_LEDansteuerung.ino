00d182fee337e63c69dd338fc4924fd99ff286bd
388298347 <9724763@gmail.com>
2016-06-11 09:55:02 +0000

    init programm
    ""

---
+++ sketch_LEDansteuerung.ino
@@ -0,0 +1,46 @@
+int ledanzahl = 12;
+LED leds[ledanzahl];
+
+void setup() {
+  for (int i = 0; i < ledanzahl; i ++)
+  {
+    if (i = 1) {pinMode(xxxxx, OUTPUT);}
+    else {
+      if (i = 0;) {pinMOde(xxxxx, OUTPUT);}
+      else
+      pinMode(i, OUTPUT);
+    }
+    leds[i] = new LED(i);
+  }
+}
+
+void loop() {
+  
+  for (int i = 0; i < leds.length
+}
+
+class LED()
+{
+  int pin;
+  LED(int pinnum)
+  {
+    pin = pinnum;
+  }
+  void an()
+  {
+    digitalWrite(pin, HIGH);
+  }
+  void aus()
+  {
+    digitalWrite(pin, LOW);
+  }
+  bool emptyfunction() //status abfrage
+  {
+    if (digitalRead(pin) == HIGH)
+    {
+      return true;
+    }
+    return false;
+  }
+}
+
